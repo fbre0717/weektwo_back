3614af1a36c22aff79995d447b295f047a9f385c
root <koh2040@naver.com>
2023-07-11 05:42:31 +0000

    0711

--- index.js
+++ index.js
@@ -14,6 +14,8 @@ const UserSchema = new mongoose.Schema({
   password : String
 });
 
+const User = mongoose.model('User', UserSchema);
+
 UserSchema.methods.comparePassword = async function (inputPassword) {
   if(inputPassword == this.password){
     return true;
@@ -22,7 +24,29 @@ UserSchema.methods.comparePassword = async function (inputPassword) {
   }
 };
 
-const User = mongoose.model('User', UserSchema);
+const TextSchema = new mongoose.Schema({
+  title: String,
+  content : String,
+});
+
+const Text = mongoose.model('text', TextSchema);
+
+
+const ProfileSchema = new mongoose.Schema({
+  userId: String,
+  birth: String,
+  hobby: String
+});
+
+const Profile = mongoose.model('profile', ProfileSchema);
+
+
+const FriendSchema = new mongoose.Schema({
+  userId: String,
+  friendList: [String]
+});
+
+const Friend = mongoose.model('friend', FriendSchema);
 
 main().catch(err => console.log(err));
 
@@ -37,7 +61,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 
 app.get('/', async (req, res) => {
-  res.send('Hello World!asd')
+  res.send('Hello World!')
 })
 
 app.post('/signup', async (req, res) => {
@@ -82,11 +106,79 @@ app.post('/signin', async (req, res) => {
 });
 
 
-app.post('/write', (req, res) => {
-  console.log(req.body);
-  res.status(200).json({ message: "일기를 작성하였습니다." });
+app.post('/write', async (req, res) => {
+  
+  try {
+    // 요청된 title로 사용자 찾기
+    const t = await Text.findOne({ title: req.body.title });
+
+    if (t) {
+      
+      res.status(400).json({ message: '이미 같은 제목의 게시글이 있습니다.' });
+      
+    } else {
+      console.log(req.body);
+      const newtext = new Text(req.body);
+      await newtext.save();
+      res.status(200).json({ message: "일기를 작성!" });
+    }
+    
+  } catch (error) {
+    console.error('Error:', error); // 오류 메시지를 콘솔에 출력
+    res.status(500).json({ message: '서버 오류로 인해 write에 실패하였습니다.' });
+  }
 });
 
+app.post('/show_profile', async (req, res) => {
+  
+  try {
+    const t = await Profile.findOne({ userId: req.body.userId });
+    
+    if (t) {
+      res.json(t);
+      // res.status(200).json({ message: '같은 userId를 갖는 유저를 찾았다' });
+      
+    } else {
+      res.status(400).json({ message: "같은 userId를 갖는 유저를 못찾음" });
+    }
+    
+  } catch (error) {
+    console.error('Error:', error); // 오류 메시지를 콘솔에 출력
+    res.status(500).json({ message: '서버 오류로 인해 show_profile에 실패하였습니다.' });
+  }
+});
+
+app.post('/add_profile', async (req, res) => {
+  
+  try {
+    const t = await Profile.findOne({ userId: req.body.userId });
+
+    if (t) {
+      
+      res.status(200).json({ message: '같은 userId를 갖는 유저가 이미 있습니다' });
+      
+    } else {
+      const newProfile = new Profile(req.body);
+      await newProfile.save();
+      res.status(400).json({ message: "유저를 추가했습니다" });
+    }
+    
+  } catch (error) {
+    console.error('Error:', error); // 오류 메시지를 콘솔에 출력
+    res.status(500).json({ message: '서버 오류로 인해 show_profile에 실패하였습니다.' });
+  }
+});
+
+app.get('/show_all_write', async (req, res) => {
+  try {
+    // 모든 데이터를 다 json으로 전달
+    const allData = await Text.find(); // Text 모델의 모든 데이터 조회
+    res.json(allData);
+  } catch (error) {
+    console.error('Error:', error); // 오류 메시지를 콘솔에 출력
+    res.status(500).json({ message: '서버 오류로 인해 shaw_all_write에 실패하였습니다.' });
+  }
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
